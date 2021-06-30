27b45089be99f937d86b57f423df0b9637b70497
Rahmad3067 <Aboozar919@gmail.com>
2021-06-30 17:36:36 +0200

    Bonus left

--- projet.js
+++ projet.js
@@ -18,98 +18,64 @@ var grid = [
     [" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 ];
 
-// .splice([3], [4])
-
-// Array.prototype.move = function (from, to) {
-//     this.splice(to, 0, this.splice(from, 1)[0]);
-//   };
-//   var ar = [1,2,3,4,5];
-// ar.move(0,);
-// alert(ar) // 2,3,4,1,5
-
 var rover = {
     direction: "N",
     x: 0,
     y: 0,
     travelLog: [],
 };
-/*
-if (rover.direction === "N") {
-    grid[rover.x][rover.y] = "N"
-}
-else if (rover.direction === "W") {
-    grid[rover.x][rover.y] = "W"
-}
-else if (rover.direction === "S") {
-    grid[rover.x][rover.y] = "S"
-}
-else if (rover.direction === "E") {
-    grid[rover.x][rover.y] = "E"
-}
-*/
 
 function turnLeft() {
 
     if (rover.direction === "N") {
         rover.direction = "W";
-        grid[rover.x][rover.y] = "W"
-
     }
     else if (rover.direction === "W") {
         rover.direction = "S";
-        grid[rover.x][rover.y] = "S"
     }
     else if (rover.direction === "S") {
         rover.direction = "E";
-        grid[rover.x][rover.y] = "E"
     }
     else if (rover.direction === "E") {
         rover.direction = "N";
-        grid[rover.x][rover.y] = "N"
     }
 };
 
 function turnRight() {
     if (rover.direction === "N") {
         rover.direction = "E";
-        grid[rover.x][rover.y] = "E"
     }
     else if (rover.direction === "E") {
         rover.direction = "S";
-        grid[rover.x][rover.y] = "S"
     }
     else if (rover.direction === "S") {
         rover.direction = "W";
-        grid[rover.x][rover.y] = "W"
     }
     else if (rover.direction === "W") {
         rover.direction = "N";
-        grid[rover.x][rover.y] = "W"
     }
 };
 
 function moveForward() {
     if (rover.direction === "N" && rover.y > 0) {
         rover.y--;
-        grid.push([" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]);
-        grid.shift();
+        grid[rover.y][rover.x] = rover.direction;
+        grid[rover.travelLog[0]][rover.travelLog[1]] = " ";
     }
     else if (rover.direction === "E" && rover.x < 9) {
         rover.x++;
-        var num = rover.y;
-        grid[num].unshift(" ");
-        grid[num].pop();
+        grid[rover.y][rover.x] = rover.direction;
+        grid[rover.travelLog[0]][rover.travelLog[1]] = " ";
     }
     else if (rover.direction === "S" && rover.y < 9) {
         rover.y++;
-        grid.unshift([" ", " ", " ", " ", " ", " ", " ", " ", " ", " "]);
-        grid.pop();
+        grid[rover.y][rover.x] = rover.direction;
+        grid[rover.travelLog[0]][rover.travelLog[1]] = " ";
     }
     else if (rover.direction === "W" && rover.x > 0) {
         rover.x--;
-        var num = rover.y;
-        grid[num].push(" ");
-        grid[num].shift();
+        grid[rover.y][rover.x] = rover.direction;
+        grid[rover.travelLog[0]][rover.travelLog[1]] = " ";
     } else {
         console.log("error: can't move further");
     }
@@ -140,7 +106,7 @@ function pilotRover() {
             }
             else if (res.str === "f") {
                 console.log("rover is moving forward");
-                rover.travelLog = [rover.x, rover.y];
+                rover.travelLog = [rover.y, rover.x];
                 moveForward(rover);
                 console.log("previous location: ", rover.travelLog)
                 console.log("current location: ", rover);
